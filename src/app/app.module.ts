854bf5a6b69a1c99991e7762b4a2e6da0f88ee60
milan <vermeulen.milan@hotmail.com>
2023-01-17 14:20:01 +0100

    Created login/input/result component and API service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InputComponent } from './input/input.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InputComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule
