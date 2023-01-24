c0ddf1aead0cd71773c0d8f8dd4989cc469755ce
milan <vermeulen.milan@hotmail.com>
2023-01-24 13:46:28 +0100

    installed chart.js/ added basic chart

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import {Chart, registerables} from 'node_modules/chart.js'
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
-export class ResultsComponent {
+export class ResultsComponent{
+constructor() {}
 
+ngOnInit(): void {
+  this.renderchart();
+}
+
+renderchart(){new Chart("linechart", {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+}
 }
