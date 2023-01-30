1f2c6b427de7d0c2431a742e8e79309992e7c2c2
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-01-30 11:25:19 +0100

    changed bearer token

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    bearerToken: '2|TwlhWYjUZ7MDX3ksz3xXyhdIqo4OrWVHOOqsPa1z'
+    bearerToken: '6|hsRPiplQEBG2s0gJ7IZidsvdgDafhkalPEPXhz6E'
   };
   
