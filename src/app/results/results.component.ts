8da6cbcb754fb1752bb2f5d1c9e6efa62de910b3
milan <vermeulen.milan@hotmail.com>
2023-01-30 13:43:04 +0100

    added comments for results component

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -15,17 +15,16 @@ ngOnInit(): void {
 }
 
 renderchart() {
+//retrieve results array from local storage
     let data: any = (localStorage.getItem('results'));
     data = JSON.parse(data);
-    // create a new array from the data object with only savings
+// create a new array from the data object with only savings
     let savings = [];
     for (let i = 0; i < data.length; i++) {
       savings.push(data[i].savings);
     }
     console.log(savings);
-
-  
-  
+ //creation of a line chart X-axis = number of years, y-axis = total savings   
   new Chart("linechart", {
       type: 'line',
       data: {
