d5571eb95c1cf4da67d0803a932d672413ca21c8
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-02-03 11:15:53 +0100

    made go back to input button for result

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -17,27 +17,27 @@ export class InputComponent {
   targetAmount: any = '';
 
   constructor(private router: Router) { }
-//calculating savings based on monthly contribtutions and interestrate
+  //calculating savings based on monthly contribtutions and interestrate
   calculateSavings() {
     let savingsByYear = [5, 10, 15, 20, 25, 30];
-// An empty array "results" is defined to store the calculated results.
+    // An empty array "results" is defined to store the calculated results.
     let results = [];
-//Calculates total savings each year by resetting this.totalSavings to this.initialAmount and using the current year from the savingsByYear array.
+    //Calculates total savings each year by resetting this.totalSavings to this.initialAmount and using the current year from the savingsByYear array.
     for (let year of savingsByYear) {
       this.totalSavings = this.initialAmount;
       this.years = year;
-//Calculates total savings over specified number of years by adding monthly contribution and interest to the total savings each month.
+      //Calculates total savings over specified number of years by adding monthly contribution and interest to the total savings each month.
       for (let i = 0; i < this.years; i++) {
         for (let j = 0; j < 12; j++) {
           this.totalSavings += this.monthlyContribution;
-          this.totalSavings += this.totalSavings * ((this.interestRate/100)/12);
+          this.totalSavings += this.totalSavings * ((this.interestRate / 100) / 12);
         }
       }
-//Pushes an object with the current year (this.years) and its corresponding savings (this.totalSavings) to the results array.
+      //Pushes an object with the current year (this.years) and its corresponding savings (this.totalSavings) to the results array.
       results.push({ year: this.years, savings: this.totalSavings });
     }
 
-//set results array in local storage
+    //set results array in local storage
     localStorage.setItem('results', JSON.stringify(results));
     console.log(localStorage.getItem('results'));
     this.router.navigate(['/results']);
