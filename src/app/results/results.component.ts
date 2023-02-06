01d911657436e4ba0815cd1073cfc0706773b55c
milan <vermeulen.milan@hotmail.com>
2023-02-06 09:40:01 +0100

    added comments to goal function

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -12,16 +12,16 @@ Chart.register(...registerables);
 export class ResultsComponent {
     target: any = (localStorage.getItem('targetAmount'));
     data: any = (localStorage.getItem('results'));
-    goals:any;
+    goals: any;
 
     constructor(private router: Router) {
         this.goals = 0;
-     }
+    }
 
     ngOnInit(): void {
         this.renderchart();
         this.goal();
-     }
+    }
 
     renderchart() {
         //retrieve results array from local storage
@@ -59,23 +59,29 @@ export class ResultsComponent {
         this.router.navigate(['/input']);
     }
 
-goal(){
-  
-  this.data = JSON.parse(this.data);
-  console.log(this.data);
-  console.log(this.target);
-    if (this.target <= this.data[0].savings) {
-    console.log("less than 5");
-    this.goals = "less than 5";
-  }
-  for (let i = 0; i < this.data.length; i++){
-    if (this.data[i].savings <= this.target && this.data[i+1].savings > this.target){
-        console.log(this.data[i].year)
-        this.goals=(this.data[i+1].year);
+    goal() {
+        // parse the data from string to JSON object
+        this.data = JSON.parse(this.data);
+        console.log(this.data);
+        console.log(this.target);
+        // check if target savings is less than the first year's savings
+        if (this.target <= this.data[0].savings) {
+            console.log("less than 5");
+            // set goals as "less than 5" if target is less than the first year's savings
+            this.goals = "less than 5";
+        }
+        // loop through the data array
+        for (let i = 0; i < this.data.length; i++) {
+            // check if the current year's savings is less than or equal to target and the next year's savings is greater than target
+            if (this.data[i].savings <= this.target && this.data[i + 1].savings > this.target) {
+                console.log(this.data[i].year)
+                // set the goals as the year that is next to the current year
+                this.goals = (this.data[i + 1].year);
+            }// check if the target savings is greater than the last year's savings
+            else if (this.target > this.data[this.data.length - 1].savings) {
+                // set goals as "more than 30" if target is more than the last year's savings
+                this.goals = 'more than 30'
+            }
+        }
     }
-    else if (this.target > this.data[this.data.length - 1].savings) {
-    this.goals = 'more than 30'
-  }
-  }  
-}
 }
